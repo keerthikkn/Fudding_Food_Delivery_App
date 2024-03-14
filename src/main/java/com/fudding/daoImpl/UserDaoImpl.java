149d5e5bb5c878e78a9602fa21ac79edcd5d3664
keerthikkn <keerthikkn2000@gmail.com>
2024-03-14 22:21:21 +0530

    first commit

---
+++ src/main/java/com/fudding/daoImpl/UserDaoImpl.java
@@ -0,0 +1,34 @@
+package com.fudding.daoImpl;
+import java.util.List;
+
+import com.fudding.dao.UserDao;
+import com.fudding.model.User;
+
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public void addUser(User user) {
+		
+	}
+
+	@Override
+	public User getUser(int userId) {
+		return null;
+	}
+
+	@Override
+	public void updateUser(User user) {
+		
+	}
+
+	@Override
+	public void deleteUser(int userId) {
+		
+	}
+
+	@Override
+	public List<User> getAllUser() {
+		return null;
+	}
+
+}
